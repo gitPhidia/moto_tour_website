a7476b88e815a17d68400a01f3eb9802dc4425b7
fanambyRaharisandratana <fanambyraharisanadratana@gmail.com>
2025-01-24 16:35:35 +0300

    commit

--- lib/moto_tour_web/live/back_live.ex
+++ lib/moto_tour_web/live/back_live.ex
@@ -1,45 +1,42 @@
 defmodule MotoTourWeb.BackLive do
   use Phoenix.LiveView
-  alias MotoTourWeb.Router.Helpers, as: Routes
-  alias MotoTour.Circuits
-  alias MotoTour.Reservations
-  import Phoenix.HTML
 
-  @impl true
   def mount(_params, _session, socket) do
-    # Initialisation d'une liste d'éléments
-    socket = assign(socket, :items, ["Item 1", "Item 2", "Item 3", "Item 4"])
-    {:ok, socket}
-  end
-
-  @impl true
-  def handle_event("reorder", %{"from" => from_index, "to" => to_index}, socket) do
-    items = socket.assigns.items
-    from_index = String.to_integer(from_index)
-    to_index = String.to_integer(to_index)
-
-    # Réorganiser la liste
-    updated_items =
-      items
-      |> List.delete_at(from_index)
-      |> List.insert_at(to_index, Enum.at(items, from_index))
-
-    {:noreply, assign(socket, :items, updated_items)}
+    # Initialisation de la liste des champs
+    {:ok, assign(socket, fields: [%{id: 1, value: ""}])}
   end
 
   def render(assigns) do
-    ~H"""
-    <div id="drag-drop-container" phx-hook="DragDrop">
-      <%= for {item, index} <- Enum.with_index(@items) do %>
-        <div
-          class="draggable-item"
-          draggable="true"
-          data-index={index}>
-          <%= item %>
+    ~L"""
+    <div class="container">
+      <h5>Le tarif comprend</h5>
+      <%= for field <- @fields do %>
+        <div class="form-group">
+          <label for="field_<%= field.id %>">prestation <%= field.id %>:</label>
+          <input type="text" id="field_<%= field.id %>" value="<%= field.value %>" class="form-control w-50" phx-change="update_field" phx-value-id="<%= field.id %>" />
         </div>
       <% end %>
+
+      <button phx-click="add_field">Ajouter un champ</button>
     </div>
     """
   end
 
+  def handle_event("add_field", _params, socket) do
+    # Ajoute un champ de texte à la liste en incrémentant l'ID
+    new_field = %{id: Enum.count(socket.assigns.fields) + 1, value: ""}
+    {:noreply, update(socket, :fields, fn fields -> fields ++ [new_field] end)}
+  end
+
+  def handle_event("update_field", %{"id" => id, "value" => value}, socket) do
+    # Met à jour la valeur du champ de texte correspondant à l'ID
+    updated_fields = Enum.map(socket.assigns.fields, fn field ->
+      if field.id == String.to_integer(id) do
+        %{field | value: value}
+      else
+        field
+      end
+    end)
+    {:noreply, assign(socket, :fields, updated_fields)}
+  end
 end

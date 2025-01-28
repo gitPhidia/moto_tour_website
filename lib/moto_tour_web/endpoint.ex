cd66c7c0fcbb3b7781d3dc5628cb2b8c7d407b46
alain-nambi <mihaja@phidia.onmicrosoft.com>
2025-01-28 14:45:44 +0300

    commit

--- lib/moto_tour_web/endpoint.ex
+++ lib/moto_tour_web/endpoint.ex
@@ -19,7 +19,7 @@ defmodule MotoTourWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :moto_tour,
-    gzip: false,
+    gzip: true,
     only: ~w(assets fonts images favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
